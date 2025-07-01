998452f3eb49a1ab18ab9452838b1dce3feb4a11
Cody Stuart <codystuart1996@gmail.com>
2025-07-01 12:37:38 -0400

    Improved game functionality in regards to AI Opponents
    game_manager.gd:
         Added a signal to emit when the game mode changes
    
    main_menu.tscn:
          Changed text of start button to "New Game"  from "Start Game" since the player is takent o a new menu rather than starting the game immediately
    
    game_start_settings.tscn:
          Added start and back buttons to the ai difficulty menu, as well as reorganized item layout
    
    game_start_settings.gd:
         Added functionality for current game mode label, improved AI difficulty UI and added code for added buttons.

--- game_manager.gd
+++ game_manager.gd
@@ -1,4 +1,10 @@
 extends Node
 
+#Game Mode Signal
+signal game_mode_changed
+
 # Manages global game state variables
-var game_mode = "pvp" # Can be pvp, easy, or hard
+var game_mode = "pvp": # Can be pvp, easy, or hard
+	set(new_value):
+		game_mode = new_value
+		game_mode_changed.emit()
--- game_start_settings.gd
+++ game_start_settings.gd
@@ -1,5 +1,8 @@
 extends Control
 
+# Game Mode Display Reference
+@onready var current_game_mode_label = $GameModeDisplayLabel
+
 # Opponent button references
 @onready var opponent_choice_panel = $PanelContainer
 @onready var human_button = $PanelContainer/VBoxContainer/HumanButton
@@ -7,21 +10,35 @@ extends Control
 @onready var back_button = $PanelContainer/VBoxContainer/BackButton
 
 # Difficulty button references
-@onready var ai_difficulty_options = $VBoxContainer/DifficultyButton
+@onready var ai_difficulty_options = $PanelContainer/VBoxContainer2/DifficultyButton
+@onready var start_button = $PanelContainer/VBoxContainer2/StartButton
+@onready var ai_back_button = $PanelContainer/VBoxContainer2/AIBackButton
 
 const SOUND_UI_CLICK = preload("res://Audio/click_002.ogg")
 
 func _ready():
+	# Connect to game mode signal
+	GameManager.game_mode_changed.connect(update_display)
+	
 	# Connect the buttons inside of the PanelContinaer
 	human_button.pressed.connect(_on_human_button_pressed)
 	ai_button.pressed.connect(_on_ai_button_pressed)
 	back_button.pressed.connect(_on_back_button_pressed)
 	
+	start_button.pressed.connect(_on_start_button_pressed)
+	ai_back_button.pressed.connect(_on_ai_back_button_pressed)
 	ai_difficulty_options.item_selected.connect(_on_difficulty_selected)
 	if GameManager.game_mode == "easy":
 		ai_difficulty_options.select(0)
 	elif GameManager.game_mode == "hard":
 		ai_difficulty_options.select(1)
+		
+func update_display():
+	# Read the current game mode from GameManager and then,
+	# Set the label's text to display the current game mode
+	var current_mode = GameManager.game_mode
+	current_game_mode_label.text = "Current Game Mode: " + current_mode.capitalize()
+	
 	
 # --- Popup panel button functions ---
 func _on_human_button_pressed():
@@ -31,21 +48,32 @@ func _on_human_button_pressed():
 	get_tree().change_scene_to_file("res://tic_tac_toe.tscn")
 	
 func _on_ai_button_pressed():
-	#GameManager.game_mode = "easy" # TODO: Fix this so that the option dropdown selection actually sets the AI difficulty.
+	# Default to easy as the game mode when AI is selected
+	GameManager.game_mode = "easy"
 	SfxManager.play(SOUND_UI_CLICK)
 	await MusicManager.fade_out_music(1.0)
-	$PanelContainer.hide()
-	$VBoxContainer.show()
-	#get_tree().change_scene_to_file("res://tic_tac_toe.tscn")
+	$PanelContainer/VBoxContainer.hide()
+	$PanelContainer/VBoxContainer2.show()
 
 func _on_back_button_pressed():
 	SfxManager.play(SOUND_UI_CLICK)
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 	
+func _on_ai_back_button_pressed():
+	SfxManager.play(SOUND_UI_CLICK)
+	$PanelContainer/VBoxContainer2.hide()
+	$PanelContainer/VBoxContainer.show()
+	
 func _on_difficulty_selected(index):
 	if index == 0:
 		GameManager.game_mode = "easy"
-		get_tree().change_scene_to_file("res://tic_tac_toe.tscn")
 	elif index == 1:
 		GameManager.game_mode = "hard"
-		get_tree().change_scene_to_file("res://tic_tac_toe.tscn")
+
+func _on_start_button_pressed():
+	SfxManager.play(SOUND_UI_CLICK)
+	if ai_difficulty_options.get_selected_id() == 0:
+		GameManager.game_mode = "easy"
+	else: # easy has an ID of 0 and hard an ID of 1, so if the ID isn't 0 (easy), then it must be 1 (hard)
+		GameManager.game_mode = "hard"
+	get_tree().change_scene_to_file("res://tic_tac_toe.tscn")
